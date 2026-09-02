c04f0abb550dc5c2750ad08f9222a6e30c3c0b72
chase-gibbs <cg514025@ohio.edu>
2026-09-02 23:26:53 +0200

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Chase Gibbs
+ *        Date: 9/2/26
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
